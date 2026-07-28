cdff4f2acc11a96853a792cdab51c1884df104e9
Dudka Pavlo <dudka.p@outlook.com>
2026-07-28 21:11:51 +0200

    рефакторінг

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import SearchBar from './components/SearchBar/SearchBar';
-import MovieGrid from './components/MovieGrid/MovieGrid';
-import Loader from './components/Loader/Loader';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
-import MovieModal from './components/MovieModal/MovieModal';
-import { fetchMovies } from './services/movieService';
-import type { Movie } from './types/movie';
-import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+import styles from './App.module.css';
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -44,9 +44,9 @@ const App = () => {
   };
 
   return (
-    <div className="app-shell">
+    <div className={styles.appShell}>
       <SearchBar onSubmit={handleSearch} />
-      <main className="main-content">
+      <main className={styles.mainContent}>
         {loading ? (
           <Loader />
         ) : error ? (
